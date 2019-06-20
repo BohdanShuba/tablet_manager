bbe78503b790b2a8c624eae32db93241f00cacbe
Bohdan <shubabohdan@gmail.com>
2019-06-20 21:09:10 +0300

    test push

--- src/main/java/com/diploma/tablet_manager/controller/RegistrationController.java
+++ src/main/java/com/diploma/tablet_manager/controller/RegistrationController.java
@@ -1,20 +1,16 @@
 package com.diploma.tablet_manager.controller;
 
-import com.diploma.tablet_manager.domain.Role;
 import com.diploma.tablet_manager.domain.User;
 import com.diploma.tablet_manager.dto.LoginDto;
 import com.diploma.tablet_manager.repos.UserRepository;
 import com.diploma.tablet_manager.service.impl.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Collections;
 import java.util.Map;
-import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -35,12 +31,8 @@ public class RegistrationController {
         return "redirect:/login";
     }
 */
-
-    private final PasswordEncoder passwordEncoder;
-    private final UserRepository userRepo;
-
     @PostMapping("/add")
-    public String addUser(LoginDto loginDto, Map<String, Object> model){
+    public String addUser(LoginDto loginDto, Map<String, Object> model) {
         User userFromDb = userServiceImpl.findUserByLogin(loginDto.getLogin());
 
         if (userFromDb != null) {
