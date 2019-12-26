b383615a55a9312a281b1248ef15c4dab0a82dd5
Bohdan <shubabohdan@gmail.com>
2019-12-26 15:21:08 +0200

    add confirmUserAccount method

--- src/main/java/com/diploma/tablet_manager/service/impl/ConfirmationTokenServiceImpl.java
+++ src/main/java/com/diploma/tablet_manager/service/impl/ConfirmationTokenServiceImpl.java
@@ -3,6 +3,7 @@ package com.diploma.tablet_manager.service.impl;
 import com.diploma.tablet_manager.domain.ConfirmationToken;
 import com.diploma.tablet_manager.domain.User;
 import com.diploma.tablet_manager.repos.ConfirmationTokenRepository;
+import com.diploma.tablet_manager.repos.UserRepository;
 import com.diploma.tablet_manager.service.ConfirmationTokenService;
 import com.diploma.tablet_manager.service.EmailSenderService;
 import lombok.RequiredArgsConstructor;
@@ -12,12 +13,25 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ConfirmationTokenServiceImpl implements ConfirmationTokenService {
     private final ConfirmationTokenRepository confirmationTokenRepository;
+    private final UserRepository userRepository;
     private final EmailSenderService emailSenderService;
 
     @Override
     public void sendConfirmationToken(User user) {
         ConfirmationToken confirmationToken = createConfirmationTokenForUser(user);
         emailSenderService.sendEmail(user.getEmail(), confirmationToken);
+    }
+
+    @Override
+    public String confirmUserAccount(String confirmationToken) {
+        ConfirmationToken token = confirmationTokenRepository.findByConfirmationToken(confirmationToken);
+        if (token != null) {
+            User user = userRepository.findByLogin(token.getUser().getLogin());
+            user.setEnabled(true);
+            userRepository.save(user);
+            return "Аккаунт подтвержден";
+        } else return "Ссылка неверна или не работает";
+
     }
 
     private ConfirmationToken createConfirmationTokenForUser(User user) {
