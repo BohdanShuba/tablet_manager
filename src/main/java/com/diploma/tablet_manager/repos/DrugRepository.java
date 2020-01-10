5ea9d9e8c138a5fdc0c76b3e0affd948d94a8568
Bohdan <shubabohdan@gmail.com>
2020-01-11 01:27:38 +0200

    add findAllByClassificationId method

--- src/main/java/com/diploma/tablet_manager/repos/DrugRepository.java
+++ src/main/java/com/diploma/tablet_manager/repos/DrugRepository.java
@@ -1,6 +1,9 @@
 package com.diploma.tablet_manager.repos;
 
 import com.diploma.tablet_manager.domain.Drug;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +14,6 @@ public interface DrugRepository extends JpaRepository<Drug, Long> {
     List<Drug> findByName(String name);
 
     Drug findById(Integer id);
+
+    Page<Drug> findAllByClassificationId(Integer id, Pageable pageRequest);
 }
