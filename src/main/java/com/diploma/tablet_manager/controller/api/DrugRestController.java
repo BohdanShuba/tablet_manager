c461c91849c825cf4cf3c5b3c4ebc6a1f59dfefc
Bohdan <shubabohdan@gmail.com>
2020-01-16 02:26:28 +0200

    add RestController

---
+++ src/main/java/com/diploma/tablet_manager/controller/api/DrugRestController.java
@@ -0,0 +1,27 @@
+package com.diploma.tablet_manager.controller.api;
+
+import com.diploma.tablet_manager.domain.UserDrug;
+import com.diploma.tablet_manager.service.impl.DrugServiceImpl;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Log4j2
+@RestController
+@RequestMapping("/api/drugs")
+@RequiredArgsConstructor
+public class DrugRestController {
+    private final DrugServiceImpl drugServiceImpl;
+
+
+    @GetMapping("/getAllDrugForUser")
+    public List<UserDrug> getAllDrugForUser() {
+        List<UserDrug> response = drugServiceImpl.getAllDrugsForUser();
+        return response;
+    }
+}
