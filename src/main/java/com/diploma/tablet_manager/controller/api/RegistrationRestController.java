66eb8b83462c133aa90cc33629375e60b34a5937
Bohdan <shubabohdan@gmail.com>
2020-02-13 21:32:57 +0200

    add Swagger to RegistrationRestController

--- src/main/java/com/diploma/tablet_manager/controller/api/RegistrationRestController.java
+++ src/main/java/com/diploma/tablet_manager/controller/api/RegistrationRestController.java
@@ -1,9 +1,13 @@
 package com.diploma.tablet_manager.controller.api;
 
 import com.diploma.tablet_manager.domain.User;
+import com.diploma.tablet_manager.dto.DrugDto;
 import com.diploma.tablet_manager.dto.UserDto;
 import com.diploma.tablet_manager.service.ConfirmationTokenService;
 import com.diploma.tablet_manager.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.*;
@@ -12,12 +16,14 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/registration")
 @RequiredArgsConstructor
+@Api(value = "Registration", description = "Operations pertaining to registration and authentication")
 public class RegistrationRestController {
     private final UserService userService;
     private final ConfirmationTokenService confirmationTokenService;
 
+    @ApiOperation(value = "Add new user", response = UserDto.class)
     @PostMapping("/add")
-    public UserDto addUser(UserDto userDto) {
+    public UserDto addUser(@ApiParam(value = "Drug object store in database table") @RequestBody UserDto userDto) {
         log.info("Add user request: " + userDto);
         User userFromDb = userService.findUserByLoginOrEmail(userDto);
         if (userFromDb != null) {
@@ -29,8 +35,9 @@ public class RegistrationRestController {
         return response;
     }
 
+    @ApiOperation(value = "Get an account confirmation", response = String.class)
     @GetMapping(value = "/confirm-account")
-    public String confirmAccount(@RequestParam("token") String confirmationToken) {
+    public String confirmAccount(@ApiParam(value = "String confirming account") @RequestParam("token") String confirmationToken) {
         log.info("Confirmation token request: " + confirmationToken);
         String response = confirmationTokenService.confirmUserAccount(confirmationToken);
         log.info("Confirmation token response: " + response);
