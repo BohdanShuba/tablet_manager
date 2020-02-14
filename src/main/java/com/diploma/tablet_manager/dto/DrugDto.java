3854a8f39a6626981d13e955ada9d609e7dc1fc1
Bohdan <shubabohdan@gmail.com>
2020-02-14 15:16:53 +0200

    add ApiModelProperty annotation to DragDto

--- src/main/java/com/diploma/tablet_manager/dto/DrugDto.java
+++ src/main/java/com/diploma/tablet_manager/dto/DrugDto.java
@@ -1,13 +1,20 @@
 package com.diploma.tablet_manager.dto;
 
-import com.diploma.tablet_manager.domain.Classification;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class DrugDto {
 
+    @ApiModelProperty(notes = "The database generated drug ID")
     private int id;
+
+    @ApiModelProperty(notes = "The drug name")
     private String name;
+
+    @ApiModelProperty(notes = "The drug instruction")
     private String instruction;
+
+    @ApiModelProperty(notes = "The drug classification Id")
     private int classificationId;
 }
