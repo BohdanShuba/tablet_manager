a68a61e8714a8bb1d57ec19da516cf783b78ae49
Bohdan <shubabohdan@gmail.com>
2020-02-18 21:09:06 +0200

    add ApiModelProperty annotation to UserDrugWithQuantityDto

--- src/main/java/com/diploma/tablet_manager/dto/UserDrugWithQuantityDto.java
+++ src/main/java/com/diploma/tablet_manager/dto/UserDrugWithQuantityDto.java
@@ -1,5 +1,6 @@
 package com.diploma.tablet_manager.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +11,16 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDrugWithQuantityDto {
+
+    @ApiModelProperty(notes = "The database generated UserDrugQuantity ID")
     private int id;
+
+    @ApiModelProperty(notes = "The drug name")
     private String name;
+
+    @ApiModelProperty(notes = "The drug instruction")
     private String instruction;
+
+    @ApiModelProperty(notes = "Set of quantity and expiration date of tablets")
     Set<DrugQuantityDto> drugsQuantityDto;
 }
