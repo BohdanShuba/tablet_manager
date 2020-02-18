460947eaf857ba7be5d5b3fff8340ed46bee5355
Bohdan <shubabohdan@gmail.com>
2020-02-18 22:05:22 +0200

    add ApiModelProperty annotation to UserDto

--- src/main/java/com/diploma/tablet_manager/dto/UserDto.java
+++ src/main/java/com/diploma/tablet_manager/dto/UserDto.java
@@ -1,10 +1,17 @@
 package com.diploma.tablet_manager.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class UserDto {
+
+    @ApiModelProperty(notes = "The user login")
     private String login;
+
+    @ApiModelProperty(notes = "The user password")
     private String password;
+
+    @ApiModelProperty(notes = "The user email")
     private String email;
 }
